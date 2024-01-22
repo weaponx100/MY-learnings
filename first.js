416656b8cc4e607f2a8a1df85b87d753b0840ff7
weaponx100 <112248208+weaponx100@users.noreply.github.com>
2024-01-22 22:34:41 +0530

    ok

--- first.js
+++ first.js
@@ -2,3 +2,12 @@ console.log("hello world");
 alert("hey");
 let name="subham";
 console.log(name);
+const student={
+    name:"subham",
+    roll_no:102,
+    cgpa:9.5,
+    age:28,
+
+};
+student["age"]=27;
+console.log(student["age"]);
